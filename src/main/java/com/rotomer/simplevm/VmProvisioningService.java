6c387ee13711c314be9d2c5018b33f86e90ada08
rotomer <rotomer9@gmail.com>
2018-07-06 23:00:17 +0300

    Simplifying the example with some use of Immutables instead of an extra protobuf message.

--- src/main/java/com/rotomer/simplevm/VmProvisioningService.java
+++ src/main/java/com/rotomer/simplevm/VmProvisioningService.java
@@ -2,7 +2,6 @@ package com.rotomer.simplevm;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.rotomer.simplevm.messages.ProvisionVmCommand;
-import com.rotomer.simplevm.messages.Vm;
 
 import java.util.UUID;
 
@@ -15,13 +14,13 @@ class VmProvisioningService {
     void provisionVm(final byte[] serializedFormat) throws InvalidProtocolBufferException {
         final ProvisionVmCommand provisionVmCommand = ProvisionVmCommand.parseFrom(serializedFormat);
 
-        final String id = nextId();
-        final Vm vm = Vm.newBuilder()
-                .setId(id)
-                .setVmSpec(provisionVmCommand.getVmSpec())
-                .setRegion(provisionVmCommand.getRegion())
-                .build();
+        doProvisioning(provisionVmCommand);
+    }
 
+    private void doProvisioning(final ProvisionVmCommand provisionVmCommand) {
+        // Doing some mock logic here for the sake of brevity:
+
+        final Vm vm = Vm.create(nextId(), provisionVmCommand.getVmSpec(), provisionVmCommand.getRegion());
         System.out.println("Provisioned vm:\n" + vm);
     }
 }
